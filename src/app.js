075eb2320bff9dfc88817a04278d5a17ff8a61e7
Arnold Parge <pargearnold@gmail.com>
2020-07-28 12:09:27 +0530

    - added time in chat data

--- src/app.js
+++ src/app.js
@@ -11,78 +11,91 @@ function mainController($scope) {
             image: './assets/images/priscilla-pp.jpeg',
             name: 'Priscilla Jioo',
             lastMessage: 'Acchhaaa',
+            lastMessageTime: '10:55 PM',
             unread: 5
         },
         {
             image: './assets/images/parge-villa-pp.jpeg',
             name: 'Parge Villa ❤💥💫⛪🏛',
             lastMessage: 'Valenine: Very ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/sylvi-pp.jpeg',
             name: 'Sylvester Parge',
             lastMessage: 'https://twitter.com/PuneCityPolice/status/1284496101706313730?s=09',
+            lastMessageTime: '10:55 PM',
             unread: 2
         },
         {
             image: './assets/images/nonstop-pp.jpeg',
             name: 'NonStop io Tech',
             lastMessage: 'Sarurabh: pee pee poo poo',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/priscilla-pp.jpeg',
             name: 'Priscilla Jioo',
             lastMessage: 'Acchhaaa',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/parge-villa-pp.jpeg',
             name: 'Parge Villa ♥💥💫⛪🏛',
             lastMessage: 'Valenine: Very ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/sylvi-pp.jpeg',
             name: 'Sylvester Parge',
             lastMessage: 'https://twitter.com/PuneCityPolice/status/1284496101706313730?s=09',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/priscilla-pp.jpeg',
             name: 'Priscilla Jioo',
             lastMessage: 'Acchhaaa',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/parge-villa-pp.jpeg',
             name: 'Parge Villa ♥💥💫⛪🏛',
             lastMessage: 'Valenine: Very ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/sylvi-pp.jpeg',
             name: 'Sylvester Parge',
             lastMessage: 'https://twitter.com/PuneCityPolice/status/1284496101706313730?s=09',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/priscilla-pp.jpeg',
             name: 'Priscilla Jioo',
             lastMessage: 'Acchhaaa',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/parge-villa-pp.jpeg',
             name: 'Parge Villa ♥💥💫⛪🏛',
             lastMessage: 'Valenine: Very ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ver happy birthday Rahul bhai. ',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
         {
             image: './assets/images/sylvi-pp.jpeg',
             name: 'Sylvester Parge',
             lastMessage: 'https://twitter.com/PuneCityPolice/status/1284496101706313730?s=09',
+            lastMessageTime: '10:55 PM',
             unread: 0
         },
     ];
--- src/components/chat-list-items/chat-list-items.js
+++ src/components/chat-list-items/chat-list-items.js
@@ -4,4 +4,5 @@ angular.module('waClone').component('chatListItem', {
         contact: '<',
         isSelected: '<'
     },
+    controllerAs: 'vm'
 });
