6ea65722b1bec6d146bde35a06de43aa7984acb7
Arnold Parge <pargearnold@gmail.com>
2020-07-30 20:42:32 +0530

    STATIC_ROOT = 'static_root' and procfile

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = 'static_root'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
