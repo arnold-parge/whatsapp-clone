67de2b2f7ae1a6f6312f53634be987aa3f2933e9
Arnold Parge <pargearnold@gmail.com>
2020-08-02 21:35:54 +0530

    Added models - Member - Chat - ChatMember - Message

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Member(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField(max_length=200)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+
+class Chat(models.Model):
+    def __str__(self):
+        return self.pk
+
+
+class ChatMember(models.Model):
+    read = models.BooleanField()
+    is_online = models.BooleanField()
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.member.name
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    text = models.TextField(max_length=10000)
+
+    def __str__(self):
+        return self.pk
